0853f71c506395a3fe7257f00d9ba752209e96da
fjjo23 <fjaramillo@steelcloud.com>
2016-08-17 16:02:02 -0400

    English plus !!

--- FrediTest/Form1.cs
+++ FrediTest/Form1.cs
@@ -24,7 +24,7 @@ namespace FrediTest
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World!");
+            MessageBox.Show("Hello World!!!");
         }
     }
 }
